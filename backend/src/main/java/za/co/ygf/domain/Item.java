c2efd4c7e290f499f2c868e977a8cea4d5a4cf32
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-09-23 23:29:39 +0200

    items for the app

---
+++ backend/src/main/java/za/co/ygf/domain/Item.java
@@ -0,0 +1,15 @@
+package za.co.ygf.domain;
+
+public class Item {
+
+    private String itemNum;
+
+    private String itemName;
+
+    private String itemDesc;
+
+    private String img;
+
+    private String price;
+
+}
