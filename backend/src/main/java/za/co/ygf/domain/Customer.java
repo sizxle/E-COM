ac806966d7966785aebd0dc4f69680cd1e34ef3b
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-09-23 23:02:12 +0200

    Basic customer and address class

---
+++ backend/src/main/java/za/co/ygf/domain/Customer.java
@@ -0,0 +1,27 @@
+package za.co.ygf.domain;
+
+import za.co.ygf.domain.common.Address;
+
+import java.time.LocalDate;
+
+public class Customer {
+
+    private String customerNum;
+
+    private String name;
+
+    private String surname;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDate dateOfBirth;
+
+    private String password;
+
+    private Address address;
+
+
+
+}
---
+++ backend/src/main/java/za/co/ygf/domain/common/Address.java
@@ -0,0 +1,5 @@
+package za.co.ygf.domain.common;
+
+public class Address {
+    
+}
