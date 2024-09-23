3e00e2b7e6526d7b5058b801ca7225c5c088a92a
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-09-23 23:30:02 +0200

    address class

--- backend/src/main/java/za/co/ygf/domain/common/Address.java
+++ backend/src/main/java/za/co/ygf/domain/common/Address.java
@@ -1,5 +1,19 @@
 package za.co.ygf.domain.common;
 
 public class Address {
-    
+
+    private String addressName;
+
+    private String city;
+
+    private String zipCode;
+
+    public Address() {
+    }
+
+    public Address(String addressName, String city, String zipCode) {
+        this.addressName = addressName;
+        this.city = city;
+        this.zipCode = zipCode;
+    }
 }
