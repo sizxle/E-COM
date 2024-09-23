b4043787416b70d6048ae19f6121fc2de8e6c57f
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-09-23 23:30:15 +0200

    Runner class for test

---
+++ backend/src/main/java/za/co/ygf/application/Runner.java
@@ -0,0 +1,11 @@
+package za.co.ygf.application;
+
+import za.co.ygf.domain.common.Address;
+
+public class Runner {
+
+    public static void main(String[] args) {
+
+        Address address= new Address();
+    }
+}
