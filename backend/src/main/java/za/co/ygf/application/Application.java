cbc6771ad160b5a8a313d87191883bdb0651964b
sthembizo.molefi <sthembizo@psybergate.co.za>
2024-09-27 19:00:40 +0200

    update my runners

--- backend/src/main/java/za/co/ygf/application/Application.java
+++ backend/src/main/java/za/co/ygf/application/Application.java
@@ -6,11 +6,10 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.ApplicationContext;
 import za.co.ygf.domain.Customer;
 import za.co.ygf.domain.Order;
-import za.co.ygf.domain.common.Address;
+import za.co.ygf.domain.Address;
 import za.co.ygf.service.CustomerService;
 import za.co.ygf.service.OrderService;
 
-import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @SpringBootApplication(scanBasePackages="za.co.ygf")
@@ -19,9 +18,11 @@ public class Application {
 
     public static void main(String[] args) {
 
-        Address address = new Address("Home", "Zeerust", "2880");
+        Customer customer = new Customer("cus111","jane","molefi","sthembizomolefi@gmail.com","0981231234", LocalDate.now(),"password");
 
-        Customer customer = new Customer("cus111","jane","molefi","sthembizomolefi@gmail.com","0981231234", LocalDate.now(),"password",address);
+        Address address = new Address("Home", "Zeerust", "2880",customer);
+
+        customer.setAddress(address);
 
         Order order = new Order("ord111",address,true);
 
